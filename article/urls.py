d2615f48abc46d6f0d5502ab0a3e910d10c878b6
wampixel <wampixel@gmail.com>
2016-11-22 21:21:04 +0100

    ajout font-awesome et modification du design

--- article/urls.py
+++ article/urls.py
@@ -6,8 +6,12 @@ app_name = 'article'
 urlpatterns = [
     #article/
     url(r'^$', views.article, name='article'),
+    #article/categorie/[0-9]*
     url(r'^categorie/(?P<categorie_id>[0-9]+)/', views.categorie, name="categorie"),
+    #article/categorie/
     url(r'^categorie/', views.all_categorie, name="categorie_all"),
+    #article/edit/
     url(r'edit/', views.edit, name="edit"),
+    #article/user_article/[0-9]*
     url(r'user_article/(?P<user_id>[0-9]*)/', views.user_article, name="user_cat"),
 ]
