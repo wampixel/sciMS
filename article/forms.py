18a8c2589925a59b347688b26c7eb63933b53157
wampixel <wampixel@gmail.com>
2016-11-29 17:02:08 +0100

    modification du modele pour article (ne pas oublier de faire makemigration et migrate)

--- article/forms.py
+++ article/forms.py
@@ -6,5 +6,5 @@ class create_article(forms.Form):
     Auteur = forms.CharField(label="Auteur")
     date = forms.DateField(label="date", widget=forms.DateInput)
     texte = forms.CharField(label="texte", widget=forms.Textarea)
-    trololo = forms.ChoiceField(label="trololo", choices=(('', '---'),
+    categories = forms.ChoiceField(label="trololo", choices=(('', '---'),
                                         ('Cc', 'Coucou')))
--- article/models.py
+++ article/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class categories(models.Model):
+    id  = models.AutoField(primary_key=True)
+    cat = models.CharField(max_length=50)
 
 class article(models.Model):
     title      = models.CharField(max_length=50)
     author     = models.CharField(max_length=50)
     date_pub   = models.DateField('date published')
-    categories = models.CharField(max_length=50)
+    categories = models.ForeignKey('categories', on_delete=models.CASCADE)
     abstract   = models.CharField(max_length=1000)
     keyword    = models.CharField(max_length=1000)
     content    = models.CharField(max_length=100)
