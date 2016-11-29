d1669ff2e26614e2d2427c0df0413c56b382605c
wampixel <wampixel@gmail.com>
2016-11-29 15:18:03 +0100

    mise en forme formulaires

--- index/forms.py
+++ index/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 
 class registration(forms.Form):
-    username = forms.CharField(max_length=100)
-    nom = forms.CharField(max_length=100)
-    prenom = forms.CharField(max_length=100)
-    passwd = forms.CharField(widget=forms.PasswordInput)
-    email = forms.CharField()
-
-class login(forms.Form):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(max_length=100, 
+                    widget=forms.TextInput(attrs={'class': 'form-control',
+                                                  'placeholder' : 'Username'}))
+    nom      = forms.CharField(max_length=100,
+                    widget=forms.TextInput(attrs={'class': 'form-control',
+                                                  'placeholder' : 'Nom'}))
+    prenom   = forms.CharField(max_length=100,
+                    widget=forms.TextInput(attrs={'class': 'form-control',
+                                                  'placeholder' : 'Prenom'}))
+    passwd   = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                 'placeholder' : 'password'}))
+    email    = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control',
+                                                             'placeholder' : 'xyz@example.fr'}))
