deeb42156e6591c0f38c2ffbcf43458b668da7ce
wampixel <wampixel@gmail.com>
2016-11-29 09:17:13 +0100

    maj repo

--- article/forms.py
+++ article/forms.py
@@ -2,8 +2,9 @@ from django import forms
 from datetime import datetime
 
 class create_article(forms.Form):
-    titre = forms.CharField()
-    Auteur = forms.CharField()
-    date = forms.DateField()
-    trololo = forms.ChoiceField(choices=(('', '---'),
+    titre = forms.CharField(label="titre")
+    Auteur = forms.CharField(label="Auteur")
+    date = forms.DateField(label="date", widget=forms.DateInput)
+    texte = forms.CharField(label="texte", widget=forms.Textarea)
+    trololo = forms.ChoiceField(label="trololo", choices=(('', '---'),
                                         ('Cc', 'Coucou')))
--- index/forms.py
+++ index/forms.py
@@ -4,5 +4,9 @@ class registration(forms.Form):
     username = forms.CharField(max_length=100)
     nom = forms.CharField(max_length=100)
     prenom = forms.CharField(max_length=100)
-    passwd = forms.CharField()
+    passwd = forms.CharField(widget=forms.PasswordInput)
     email = forms.CharField()
+
+class login(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
